efa2758a252f5611b4f3b5fd8a387d16a02d9a6a
Bastian Ruppert <Bastian.Ruppert@Sewerin.com>
2017-11-14 09:05:37 +0100

    pfad angepasst
    modified:   mount_temp.sh

--- mount_temp.sh
+++ mount_temp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo vmhgfs-fuse -o allow_other -o auto_unmount -o uid=1000 -o gid=1000 .host:/M /mnt/hgfs/M
+sudo vmhgfs-fuse -o allow_other -o auto_unmount -o uid=1000 -o gid=1000 .host:/temp /mnt/hgfs/temp
