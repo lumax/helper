cbbbfb48feef2801c0c589b5251c34e258c37a17
Bastian Ruppert <Bastian.Ruppert@Sewerin.com>
2018-09-24 11:05:55 +0200

    added describeGits.sh
    new file:   describeGits.sh

---
+++ describeGits.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Bastian Ruppert 24.09.2018
+
+#Zeigt die Version und den Commit-Hash von git-Repositories an.
+#Diese Informationen können dann in Yocto-Rezepten verwendet werden. 
+
+REPOS="ea143g_a200_firmware_gui ea143g_c200_firmware_gui
+       ea143g_resources syslinkcorr  w1controller ea143g_appswitcher
+       DspTransfer"
+
+stage_info()
+{
+    for name in $REPOS
+    do
+	echo " "
+	cd $name
+	echo "$name:"
+	echo "#`git describe --dirty`"
+	echo "SRCREV = \"`git log -1 --pretty=format:"%H"`\""
+	cd ..
+    done
+}
+
+stage_help()
+{
+    echo "zeigt repo Infos für yocto Rezepte an REPOS:"
+    for name in $REPOS
+    do
+	echo $name
+    done
+}
+
+die()
+{
+    echo $*
+    exit 1
+}
+
+# When no stage is specified, do all of them or help
+if [ $# -eq 0 ]; then
+    set -- info
+fi
+
+while [ $# -gt 0 ]; do
+    case $1 in
+		 info|help)
+		     eval "stage_$1"
+		     ;;
+		 *)
+		     echo "unknown stage '$1'" >&2
+		     die
+		     ;;
+    esac
+    shift
+done
