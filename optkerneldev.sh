5e2eaba9bc6cddd34a2fe2d5a947ecc6ae1bcd6a
Bastian Ruppert <Bastian.Ruppert@Sewerin.com>
2019-06-19 06:57:00 +0200

    added optkerneldev.sh
    new file:   optkerneldev.sh

---
+++ optkerneldev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gnome-terminal --geometry=96x46+0+0 --working-directory=/home/sewerin/kernel/psp/linux-omapl1 &#--command=".exportskernel.sh &"
+#gnome-terminal --geometry=96x30+790+0
+gnome-terminal --geometry=96x27+790+562 --working-directory=/home/sewerin/tftp &
