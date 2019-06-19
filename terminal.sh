4ca04385375a46747f82450dc983f8190d2d4177
Bastian Ruppert <Bastian.Ruppert@Sewerin.com>
2019-06-19 06:57:42 +0200

    terminal.sh: Pfad geaendert
    modified:   terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gnome-terminal --geometry=84x11+18+0 --working-directory=/opt/dbrfsb &
 gnome-terminal --geometry=84x37+18+340 --command="kermit .uart_kermrc -c" &
-gnome-terminal --geometry=90x25+860+0 --working-directory=/opt/xenvlumax &
-gnome-terminal --geometry=90x28+860+562 --working-directory=/opt/xenvlumax &
+gnome-terminal --geometry=90x25+860+0 --working-directory=/opt/source &
+gnome-terminal --geometry=90x28+860+562 --working-directory=/opt/source &
